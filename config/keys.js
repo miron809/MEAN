d386ca75eed84c2ed3339b0df1a392a08d9a3e49
Dmytro Myronenko <dim@uain.su>
2020-06-26 15:30:10 +0300

    added register method

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:AWpkIVg9Qa2BkCed@cluster0-fjrfd.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://admin:AWpkIVg9Qa2BkCed@cluster0-fjrfd.mongodb.net/meanDB?retryWrites=true&w=majority'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 module.exports.login = (req, res) => {
   res.status(200).json({
     login: {
@@ -7,8 +10,28 @@ module.exports.login = (req, res) => {
   })
 };
 
-module.exports.register = (req, res) => {
-  res.status(200).json({
-    register: true
-  })
+module.exports.register = async (req, res) => {
+  const candidate = await User.findOne({email: req.body.email});
+
+  if (candidate) {
+    res.status(409).json({
+      message: 'Exist email'
+    })
+  } else {
+    const salt = bcrypt.genSaltSync(10);
+    const password = req.body.password;
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    });
+
+    try {
+      await user.save();
+      res.status(201).json(user)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  user.save().then(() => console.log('User created'));
 };
