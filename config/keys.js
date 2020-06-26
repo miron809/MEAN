caad4ab46e4885f952f100622f3bcfc713bbfd34
Dmytro Myronenko <dim@uain.su>
2020-06-26 12:29:34 +0300

    added mongoDB features

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin:AWpkIVg9Qa2BkCed@cluster0-fjrfd.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
---
+++ models/Position.js
@@ -0,0 +1,23 @@
+const mongoose = required('mongoose');
+const Schema = mongoose.Schema;
+
+const positionSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  category: {
+    ref: 'categories',
+    type: Schema.Types.ObjectID
+  },
+  user: {
+    ref: 'users',
+    type: Schema.Types.ObjectID
+  }
+});
+
+module.exports = mongoose.model('positions', positionSchema);
