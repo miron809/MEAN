0cbaac82f28a43a1bd76d498dd8ca3e453920897
Dmytro Myronenko <dim@uain.su>
2020-06-30 11:58:24 +0300

    added login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,32 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = (req, res) => {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+module.exports.login = async (req, res) => {
+  const candidate = await User.findOne({email: req.body.email});
+
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+    if (passwordResult) {
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: 60 * 60});
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        message: 'Invalid password'
+      })
     }
-  })
+  } else {
+    res.status(404).json({
+      message: 'User does not exist'
+    })
+  }
 };
 
 module.exports.register = async (req, res) => {
