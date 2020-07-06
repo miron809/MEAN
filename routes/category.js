9e604b2a3da87ed7eeb67d3a14972ed357ae1b23
Dmytro Myronenko <dim@uain.su>
2020-07-06 17:54:29 +0300

    updated category API

--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const controller = require('../controllers/category');
 const passport = require('passport');
+const upload = require('../middleware/upload');
+const controller = require('../controllers/category');
 const router = express.Router();
 
 require('../middleware/passport')(passport);
 
 router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
-router.get('/:id', controller.getById);
-router.post('/', controller.create);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.remove);
+router.get('/:id', passport.authenticate('jwt', {session: false}), controller.getById);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), controller.update);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
 
 module.exports = router;
