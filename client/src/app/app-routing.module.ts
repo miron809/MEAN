6c985088f2eb9f9d93f1d97c75e3eb9222329743
Dmytro Myronenko <dim@uain.su>
2020-07-13 11:35:51 +0300

    added base layouts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginPageComponent
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginPageComponent
+      },
+      {
+        path: 'register',
+        component: RegisterPageComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: SiteLayoutComponent,
+    children: []
   }
   ];
 
