e57343b709c7a67172adff8b2f1834a6bda305cc
Dmytro Myronenko <dim@uain.su>
2020-07-13 15:45:46 +0300

    added auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
@@ -21,7 +21,8 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  unsubscriber = new Subject();
+
   form: FormGroup;
 
-  constructor() { }
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getQueryParams();
     this.buildForm();
+  }
+
+  getQueryParams() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+
+      } else if (params['accessDenied']) {
+
+      }
+    })
   }
 
   buildForm() {
@@ -23,7 +42,21 @@ export class LoginPageComponent implements OnInit {
   }
 
   onSubmit() {
+    this.auth.login(this.form.value)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe(
+        () => {
+          this.router.navigate(['/overview'])
+        },
+        error => {
+          console.log(error)
+        }
+      )
+  }
 
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
   }
 
 }
