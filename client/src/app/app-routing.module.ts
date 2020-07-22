d153b9cb19dbafc63508d023a8542e4111552b09
Dmytro Myronenko <dim@uain.su>
2020-07-22 18:45:35 +0300

    added order-pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.co
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
-import { OrderPageComponent } from './order-page/order-page.component';
 import { HistoryPageComponent } from './history-page/history-page.component';
 import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
 import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { OrderCategoriesComponent } from './order-page/order-categories/order-categories.component';
+import { OrderPositionsComponent } from './order-page/order-positions/order-positions.component';
 
 
 const routes: Routes = [
@@ -41,7 +43,10 @@ const routes: Routes = [
       {path: 'overview', component: OverviewPageComponent},
       {path: 'analytics', component: AnalyticsPageComponent},
       {path: 'history', component: HistoryPageComponent},
-      {path: 'order', component: OrderPageComponent},
+      {path: 'order', component: OrderPageComponent, children: [
+          {path: '', component: OrderCategoriesComponent},
+          {path: ':id', component: OrderPositionsComponent}
+        ]},
       {path: 'categories', component: CategoriesPageComponent},
       {path: 'categories/new', component: CategoriesFormComponent},
       {path: 'categories/:id', component: CategoriesFormComponent}
--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.scss']
+  styleUrls: ['./order-page.component.scss'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  isRoot: boolean = true;
+  @ViewChild('modal') modalRef: ElementRef;
+  modal: MaterialInstance;
+
+  constructor(private router: Router,
+              private orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order'
+      }
+    })
   }
 
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.modal.close();
+  }
 }
