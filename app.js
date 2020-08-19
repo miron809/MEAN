8756cbde6a2e5a86b51c00cafb1dbe30c96a87b1
Dmytro Myronenko <dim@uain.su>
2020-08-19 10:06:28 +0300

    prod optimization

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -30,6 +31,18 @@ app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist/client'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname, 'client', 'dist', 'client', 'index.html'
+      )
+    )
+  })
+}
 
 
 
---
+++ config/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin:AWpkIVg9Qa2BkCed@cluster0-fjrfd.mongodb.net/meanDB?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
+};
